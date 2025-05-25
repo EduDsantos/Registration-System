e4c764c3cd315f4f48fae27a55ff354ec8f7978a
Eduardo dos Santos <111947576+EduDsantos@users.noreply.github.com>
2025-05-25 11:12:14 -0300

    começando a fazer a tela de alunos

--- frontend/src/pages/Alunos/Alunos.jsx
+++ frontend/src/pages/Alunos/Alunos.jsx
@@ -1,10 +1,72 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import axios from 'axios'
 import './Alunos.css'
 
-export default function Alunos(){
-    return(
+
+
+export default function Alunos() {
+
+    const [alunos, setAlunos] = useState([])
+    const [loading, setLoading] = useState(true)
+
+    useEffect(() => {
+        const fetchAlunos = async () => {
+            try {
+                const token = localStorage.getItem('token')
+                const response = await axios.get('http://localhost:3000/alunos', {
+                    headers: {
+                        Authorization: `Bearer ${token}`
+                    }
+                })
+
+                setAlunos(response.data)
+            } catch (error) {
+                console.log('Erro ao buscar alunos:', error)
+            } finally {
+                setLoading(false)
+            }
+        }
+
+        fetchAlunos()
+    }, [])
+    if (loading) return <p> Carregando... </p>
+
+    return (
         <div>
-            teste
+            <h2>Lista de Alunos</h2>
+            {alunos.length === 0 ? (
+                <p>Nenhum aluno cadastrado</p>
+            ) : (
+                <table border="1">
+                    <thead>
+                        <tr>
+                            {/* <th>ID</th> */}
+                            <th>Nome</th>
+                            <th>Faixa</th>
+                            <th>Status</th>
+                            <th>Ações</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {alunos.map((aluno) => {
+                            console.log(aluno);  // <-- AQUI!
+
+                            return (
+                                <tr key={aluno._id}>
+                                    <td>{aluno.nome}</td>
+                                    <td>{aluno.faixa}</td>
+                                    <td>{aluno.status}</td>
+                                    <td>
+                                        <button>Editar</button>
+                                        <button>Excluir</button>
+                                    </td>
+                                </tr>
+                            );
+                        })}
+                    </tbody>
+                </table>
+
+            )}
         </div>
-    )
-}
+    );
+};
